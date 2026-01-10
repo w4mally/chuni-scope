c5f12d2567551da2238745d026a06227f39977b3
w4mally <kazuma246r@gmail.com>
2026-01-10 15:39:27 +0900

    update: デバック用変数をコメントアウト

--- src/scraper/scraper.js
+++ src/scraper/scraper.js
@@ -62,6 +62,7 @@
 		stElement.innerText = 'データ送信中...';
 
 		const baseUrl = 'https://chuni-scope.vercel.app';
+		// const baseUrl = 'http://localhost:3000/';
 
 		try {
 			const response = await fetch(`${baseUrl}/api/save`, {
