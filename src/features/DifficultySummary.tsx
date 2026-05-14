cad1e6b8ab90f36329e93ccb88405d340bfc9e9a
w4mally <kazuma246r@gmail.com>
2026-05-14 22:28:29 +0900

    update: レベル16に対応

--- src/features/DifficultySummary.tsx
+++ src/features/DifficultySummary.tsx
@@ -7,7 +7,7 @@ interface DifficultySummaryProps {
 	isExport?: boolean;
 }
 
-const levels = ['10', '10+', '11', '11+', '12', '12+', '13', '13+', '14', '14+', '15', '15+'];
+const levels = ['10', '10+', '11', '11+', '12', '12+', '13', '13+', '14', '14+', '15', '15+', '16'];
 
 const DifficultySummary = ({ data , isExport = false}: DifficultySummaryProps) => {
 	const Style = isExport
